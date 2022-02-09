9ae1d2a610967565828b4601a8ec93ddd81fdbed
CLBoisvert <christopherboisvert.student@careerdevs.com>
2022-02-08 22:55:34 -0500

    Completed part 1 of 2 for the Say Hello Challenge

--- src/com/careerdevs/sayhello/SayHello.java
+++ src/com/careerdevs/sayhello/SayHello.java
@@ -25,4 +25,24 @@ public class SayHello {
         System.out.println("Your last name is " + userLastName);
         System.out.println(userFirstName + " " + userLastName + " it is nice to meet you");
     }
+
+    public static void fName(){
+        //need a new scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //ask a question
+        System.out.println("What is your first name?");
+
+        //read and make input a variable
+        String userFName = scanner.nextLine();
+
+        //ask another question
+        System.out.println("What is your last name?");
+
+        //read and input
+        String userLName = scanner.nextLine();
+
+        //output both
+        System.out.println(userFName + " " + userLName + "is it nice to meet you!");
+    }
 }
