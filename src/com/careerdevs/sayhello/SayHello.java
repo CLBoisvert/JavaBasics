584b46f9346469f5e4deb010d05ade3f35c1e1e3
CLBoisvert <christopherboisvert.student@careerdevs.com>
2022-02-10 19:10:16 -0500

    Completed part 2 of 2 for SayHello Challenge

--- src/com/careerdevs/sayhello/SayHello.java
+++ src/com/careerdevs/sayhello/SayHello.java
@@ -4,29 +4,10 @@ import java.util.Scanner;
 
 public class SayHello {
     public static void main(String [] args){
-        //write code here
-        //make scanner to read user input
-        Scanner scanner = new Scanner(System.in);
-
-        //now ask the user a question
-        System.out.println("What is your first name?");
-
-        //read users input, and store it in a variable
-        String userFirstName = scanner.nextLine();
-
-        //now ask for last name
-        System.out.println("What is your last name?");
-
-        //variable for user last name
-        String userLastName = scanner.nextLine();
-
-        //display message with user input
-        System.out.println(userFirstName);
-        System.out.println("Your last name is " + userLastName);
-        System.out.println(userFirstName + " " + userLastName + " it is nice to meet you");
+        fullName();
     }
 
-    public static void fName(){
+    public static void fullName(){
         //need a new scanner
         Scanner scanner = new Scanner(System.in);
 
